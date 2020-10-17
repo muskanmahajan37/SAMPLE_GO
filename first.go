3954d927b2756016834a3afff2d9231b9cfbd9d2
AG16000 <rohankumar997>
2020-10-17 20:49:22 +0530

    sample go programme

---
+++ first.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// var i int = 12
+	// j := 16
+	// fmt.Println("hello world")
+	// fmt.Println("sup man")
+	// fmt.Println("yo dude")
+	// fmt.Println(i)
+	// fmt.Printf("%v , %T", j, j)
+
+}
